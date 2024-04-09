6ca0a8fd5c6770ec317b987238fe5c941a70d1dc
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-04-09 15:11:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/gdph', async (req, res) => {
     try {
         // Set headers to mimic Mozilla browser
         const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
             'Referer': 'https://gdph.ps.fhgdps.com/',
             'Origin': 'https://gdph.ps.fhgdps.com/',
             'Content-Type': 'application/x-www-form-urlencoded'
